7ef6d3a013ea7c7564372e46e411d15a10086930
Asmae <Gunisasmae@gmail.com>
2022-03-28 23:01:18 +0000

    asmae : creating e_learning_db

--- src/main/java/com/ensias/gl/elearning/webservice/ElearningWebServiceApplication.java
+++ src/main/java/com/ensias/gl/elearning/webservice/ElearningWebServiceApplication.java
@@ -1,4 +1,4 @@
-package com.ensias.gl.elearning.elearning_web_service;
+package com.ensias.gl.elearning.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
