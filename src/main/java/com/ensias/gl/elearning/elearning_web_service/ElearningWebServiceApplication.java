6cb6168a171ae592e002baa622bf7ff88fba32d9
Asmae <Gunisasmae@gmail.com>
2022-03-28 22:51:34 +0000

    initial comit

---
+++ src/main/java/com/ensias/gl/elearning/elearning_web_service/ElearningWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ensias.gl.elearning.elearning_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearningWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElearningWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ensias/gl/elearning/elearning_web_service/ElearningWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensias.gl.elearning.elearning_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
